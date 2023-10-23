32c1fcba2f486f571a4f5d9176b2066560288875
Paulo <paulolucas.5000@gmail.com>
2023-10-23 21:43:27 +0000

    Vasco

---
+++ poo/projeto009/main.py
@@ -0,0 +1,11 @@
+import fastapi
+
+menu = [  {   'id': 1,        'name': 'coffee',        'price': 2.5     },
+        {        'id': 2,        'name': 'cake',        'price': 10    },
+        {        'id': 3,        'name': 'tea',        'price': 3.2    },
+        {        'id': 4,        'name': 'croissant',        'price': 5.79    }
+]
+app = fastapi.FastAPI()
+@app.get("/")
+def hello_world_root():
+    return {"Turma": "Backend"}
